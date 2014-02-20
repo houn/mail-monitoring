2d276a45384736e4c243a44fc7f14ec4520e7405
houn <good.sergio@gmail.com>
2014-02-20 11:22:15 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailMonitoring::Application.config.secret_token = '57b6b89bcfa70cc73530d6535fbdf1fb761bf5880f93b145f06860ab5fda8e68120bfac636221a2e220ecaef527165368013e7056c1ab6eda628d4f29c9a2b02'
